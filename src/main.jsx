322ba8ef798d2be4bcbb8404b751d3760177c180
Anwesha Bhatt <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-08-19 15:39:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI
     }}
   >
-    <App apiUrl={'https://clock-backend-tuac.onrender.com'} demo={false} />
+    <App apiUrl={'https://clock-backend-recent.onrender.com'} demo={false} />
   </Auth0Provider>
 );
